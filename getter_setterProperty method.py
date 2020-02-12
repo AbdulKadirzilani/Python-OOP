5805ceaa30830e46ee42af73f00629dd45b4e95b
zilani <abdulkadir14015@gmail.com>
2020-02-12 13:06:46 +0600

    setter and getter method 1

---
+++ getter_setterProperty method.py
@@ -0,0 +1,26 @@
+class Student:
+
+    def setName(self,a):
+        self.__name = a
+    def getName(self):
+        return self.__name
+
+    def setMarks(self,b):
+        self.__marks = b
+
+    def getMarks(self):
+         return self.__marks
+
+    def display(self):
+        print(self.__name)
+        print(self.__marks)
+'''
+
+    def display(self):
+        print(self.getName())
+        print(self.getMarks())
+'''
+s1 = Student()
+s1.setName("softlet")
+s1.setMarks(50)
+s1.display()
