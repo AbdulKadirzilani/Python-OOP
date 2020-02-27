c2dc0d1a0b0338f1b825eeae4833815ee392d8be
zilani <abdulkadir14015@gmail.com>
2020-02-27 18:07:32 +0600

    map use for iterate obj like list

--- map_anisul.py
+++ map_anisul.py
@@ -2,9 +2,15 @@
 def sqr(i):
     return i*i
 
-
-num = [1,2,3]
-
+num=[1,2,3]
 a =list(map(sqr,num))
+
+
+#Toatl ekta list ke parameter hisabe iterate korte hole map use korte hobe
+
+#Don't work
+# num=[1,2,3]
+#
+# a=sqr(num)
 
 print(a)
