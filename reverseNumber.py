ec1e0dac4a537aa6849101220a9cb72b44202beb
zilani <abdulkadir14015@gmail.com>
2020-03-03 01:18:16 +0600

    reverse using loop

---
+++ reverseNumber.py
@@ -0,0 +1,32 @@
+a ="mum"
+a=list(a)
+#print(a)
+li=[]
+# self.s2.append(self.s1[-1])
+for i in range(1,len(a)+1):
+    li.append(a[-i])
+
+print(li)
+
+
+if a ==li:
+    print("palindrome")
+else:
+    print("n")
+
+
+# c="v"
+# v = ["v"]
+# print(v[0])
+# print(type(v))
+# d=str(v)
+# if c==d:
+#     print("l")
+
+# c="mam"
+# c=str(c)
+# d=["mam"]
+# d=str(d)
+# if c==d:
+#     print(f"equel c={c}, d ={d}")
+
