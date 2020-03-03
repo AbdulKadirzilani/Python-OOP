7ee92eaf3793336b72d31d0bbc136f07c27a2c1d
zilani <abdulkadir14015@gmail.com>
2020-03-03 13:44:47 +0600

    print reverse order

--- 10 To 1.py
+++ 10 To 1.py
@@ -5,6 +5,12 @@ for i in range(10,0,-1):
 #new line paoya porjonto cholte takhbe
 
 print('\n')
-#1 2 3 4 5 6 7 8 9
+#1 2 3 4 5 6 7 8
 for j in range(1,10,1):
     print(j,end=' ')
+
+
+
+# 10 9 8 7 6 5 4 3 2 1
+#
+# 1 2 3 4 5 6 7 8 9
