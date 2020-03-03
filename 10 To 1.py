228cda68c886ad6f2562d147ac24731fd18d46cc
zilani <abdulkadir14015@gmail.com>
2020-03-03 13:37:40 +0600

    add functionality

---
+++ 10 To 1.py
@@ -0,0 +1,10 @@
+# 10 9 8 7 6 5 4 3 2 1
+for i in range(10,0,-1):
+    print(i,end= ' ')
+
+#new line paoya porjonto cholte takhbe
+
+print('\n')
+#1 2 3 4 5 6 7 8 9
+for j in range(1,10,1):
+    print(j,end=' ')
--- endFunction.py
+++ endFunction.py
@@ -2,4 +2,7 @@
 for i in range(6,0,-1):
     print(i,end=' ')
 
-#new line paoya porjonto cholte takhbe
+#new line paoya porjonto cholte takhbe, newline pele break jmn
+
+print('\n')
+print()
