029a5d46db49094646a403af3b010aaf42b35830
zilani <abdulkadir14015@gmail.com>
2020-03-07 16:31:04 +0600

    sss

---
+++ commnSubstring.py
@@ -0,0 +1,15 @@
+s = "hellow world"
+
+if 'wo' in s:
+    print("found")
+
+#
+# s1="hd"
+# s2= []
+# for i in s:
+#     for j in s1:
+#         if j in i:
+#             s2.append(j)
+# #print(s2)
+# if str(s2)==s1:
+#     print("found")
