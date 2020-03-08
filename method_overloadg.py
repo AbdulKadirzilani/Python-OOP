3f68fab0a6fb89f699ea797082fe9e4e73adf706
zilani <abdulkadir14015@gmail.com>
2020-03-09 00:16:03 +0600

    same class 2 have to method do't support

--- method_overloadg.py
+++ method_overloadg.py
@@ -1,8 +1,15 @@
-def sum(a,b,c=0):
-    return a+b+c
+#same method have one class is call overloading
+#but python do't support its/ this technique work different
 
+def sum(a=0,b=0,c=0,d=0):
+
+    return a+b+c+d
+
+print(2)
 print(sum(2,3))
 print(sum(2,1,6))
+
+print(sum(1,1,1,1))
 
 
 
