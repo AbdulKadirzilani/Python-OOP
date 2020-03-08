211aa5c9ef44a79b62ddf91de7116c2853b99031
zilani <abdulkadir14015@gmail.com>
2020-03-08 23:44:50 +0600

    super class

---
+++ method_overiding.py
@@ -0,0 +1,18 @@
+# Base class A
+# Base class B
+#super class
+
+class A:
+    def Phone(self):
+
+        print("Base class A")
+
+class B(A):
+
+    def Phone(self):
+        super().Phone()
+        print("Base class B")
+
+x = B()
+
+print(x.Phone())
