ea0f962e419132cfa0c72dbeb008694321e24f66
zilani <abdulkadir14015@gmail.com>
2020-03-08 12:06:14 +0600

    triangle

--- pyramid_triangle.py
+++ pyramid_triangle.py
@@ -1,17 +1,22 @@
 # Python 3.x code to demonstrate star pattern
 
-# Function to demonstrate printing pattern triangle
+#
+#    *
+#   * *
+#  * * *
+# * * * *
+
+
 def triangle(n):
     # number of spaces
     k = 2 * n - 2
     for i in range(0, n):
         for j in range(0, k):
-            print(end=
-                  " ")
+            print('',end=" ")
 
             # decrementing k after each loop
         k = k - 1
-        for j in range(0, i + 1):
+        for j in range(0, i + 1):   #0,1=1
             # printing stars
             print("* ", end="")
 
